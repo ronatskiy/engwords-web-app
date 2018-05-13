62ce1a2a025a771e6107fccf8bc0ee7d50ee03ff
ronatskiy <r.onatskiy@gmail.com>
2018-05-13 21:06:42 +0300

    Reformat code using prettier

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -29,7 +29,7 @@ function getProgressWidth(currWord, wordCount) {
 }
 
 function navigateButtonHandler($scope, hideTranslateHandler) {
-	$scope.prev = function () {
+	$scope.prev = function() {
 		if (hideTranslateHandler) {
 			hideTranslateHandler();
 		}
@@ -39,7 +39,7 @@ function navigateButtonHandler($scope, hideTranslateHandler) {
 		reloadProgress($scope);
 	};
 
-	$scope.next = function () {
+	$scope.next = function() {
 		if (hideTranslateHandler) {
 			hideTranslateHandler();
 		}
@@ -57,35 +57,37 @@ function initWords($scope, data) {
 }
 
 memorizeApp.factory("LessonsData", [
-        "$resource", function ($resource) {
-        	return $resource("store/:id.json", {});
-        }]
-);
+	"$resource",
+	function($resource) {
+		return $resource("store/:id.json", {});
+	},
+]);
 
 memorizeApp.factory("IrregularVerbsData", [
-        "$resource", function ($resource) {
-        	return $resource("store/irregular-verbs.json", {});
-        }]
-);
+	"$resource",
+	function($resource) {
+		return $resource("store/irregular-verbs.json", {});
+	},
+]);
 
 memorizeApp.controller("mainCtrl", [
-    "$scope",
-    function ($scope) {
-    	$scope.firstLesson = 15;
-    	$scope.currLesson = $scope.firstLesson;
-    	$scope.lessons = _.range($scope.firstLesson, 28);
-    	$scope.main = {
-    		isBrandNameShown: true,
-    		firstLesson: $scope.firstLesson
-    	};
+	"$scope",
+	function($scope) {
+		$scope.firstLesson = 15;
+		$scope.currLesson = $scope.firstLesson;
+		$scope.lessons = _.range($scope.firstLesson, 28);
+		$scope.main = {
+			isBrandNameShown: true,
+			firstLesson: $scope.firstLesson,
+		};
 
-    	$scope.exit = function () {
-    		if ($scope.main.isBrandNameShown && navigator) {
-    			// TODO: uncomment me in the cordoba app
-    			// navigator.app.exitApp();
-    		}
-    	}
-    }
+		$scope.exit = function() {
+			if ($scope.main.isBrandNameShown && navigator) {
+				// TODO: uncomment me in the cordoba app
+				// navigator.app.exitApp();
+			}
+		};
+	},
 ]);
 
 function BtnComponent(title, path, bgColor) {
@@ -95,138 +97,153 @@ function BtnComponent(title, path, bgColor) {
 }
 
 memorizeApp.controller("startPageCtrl", [
-    "$scope",
-    function ($scope) {
-    	$scope.title = "Привет!";
-    	$scope.message = "Чем сегодня займёмся?";
-    	$scope.sections = [
-            new BtnComponent("Уроки", "#/list/lesson/", "btn-material-green btn-lg"),
-            new BtnComponent("Тренировка", "#/list/training", "btn-material-light-blue btn-lg"),
-            new BtnComponent("Разр. Фр. Глаголы", "#/detail-view/separable-verbs", "btn-material-cyan btn-lg"),
-            new BtnComponent("Неразр. Фр. Глаголы", "#/detail-view/inseparable-verbs", "btn-material-indigo btn-lg"),
-            new BtnComponent("Неправ. Глаголы", "#/irregular-verbs", "btn-material-light-green btn-lg")
-    	];
-    	$scope.main.isBrandNameShown = true;
-    }
+	"$scope",
+	function($scope) {
+		$scope.title = "Привет!";
+		$scope.message = "Чем сегодня займёмся?";
+		$scope.sections = [
+			new BtnComponent("Уроки", "#/list/lesson/", "btn-material-green btn-lg"),
+			new BtnComponent("Тренировка", "#/list/training", "btn-material-light-blue btn-lg"),
+			new BtnComponent("Разр. Фр. Глаголы", "#/detail-view/separable-verbs", "btn-material-cyan btn-lg"),
+			new BtnComponent("Неразр. Фр. Глаголы", "#/detail-view/inseparable-verbs", "btn-material-indigo btn-lg"),
+			new BtnComponent("Неправ. Глаголы", "#/irregular-verbs", "btn-material-light-green btn-lg"),
+		];
+		$scope.main.isBrandNameShown = true;
+	},
 ]);
 
 memorizeApp.controller("detailViewCtrl", [
-    "$scope", "$routeParams", "LessonsData",
-    function ($scope, $routeParams, LessonsData) {
-    	$scope.main.isBrandNameShown = false;
+	"$scope",
+	"$routeParams",
+	"LessonsData",
+	function($scope, $routeParams, LessonsData) {
+		$scope.main.isBrandNameShown = false;
 
-    	var storeFileId = $routeParams.id,
-        btn1Path = "",
-        btn2Path = "";
+		var storeFileId = $routeParams.id,
+			btn1Path = "",
+			btn2Path = "";
 
-    	$scope.title = setTitle(storeFileId);
+		$scope.title = setTitle(storeFileId);
 
-    	btn1Path = "#/learning/" + storeFileId + "/learn";
-    	btn2Path = "#/learning/" + storeFileId + "/training";
+		btn1Path = "#/learning/" + storeFileId + "/learn";
+		btn2Path = "#/learning/" + storeFileId + "/training";
 
-    	$scope.sections = [
-            new BtnComponent("Учить!", btn1Path, "btn-warning btn-lg"),
-            new BtnComponent("Тренировать", btn2Path, "btn-info btn-lg")
-    	];
+		$scope.sections = [
+			new BtnComponent("Учить!", btn1Path, "btn-warning btn-lg"),
+			new BtnComponent("Тренировать", btn2Path, "btn-info btn-lg"),
+		];
 
-    	LessonsData.query({ id: storeFileId }, function (data) {
-    		$scope.words = data;
-    		$scope.message = "Слов на сегодня " + $scope.words.length;
-    	});
-    }
+		LessonsData.query({ id: storeFileId }, function(data) {
+			$scope.words = data;
+			$scope.message = "Слов на сегодня " + $scope.words.length;
+		});
+	},
 ]);
 
 memorizeApp.controller("irregularVerbsCtrl", [
-    "$scope", "$routeParams", "IrregularVerbsData",
-    function ($scope, $routeParams, IrregularVerbsData) {
-    	$scope.main.isBrandNameShown = false;
+	"$scope",
+	"$routeParams",
+	"IrregularVerbsData",
+	function($scope, $routeParams, IrregularVerbsData) {
+		$scope.main.isBrandNameShown = false;
 
-    	var btn1Path = "", btn2Path = "";
+		var btn1Path = "",
+			btn2Path = "";
 
-    	$scope.title = "Неправильные Глаголы";
+		$scope.title = "Неправильные Глаголы";
 
-    	btn1Path = "#/learning/irregular-verbs/learn";
-    	btn2Path = "#/learning/irregular-verbs/training";
+		btn1Path = "#/learning/irregular-verbs/learn";
+		btn2Path = "#/learning/irregular-verbs/training";
 
-    	$scope.sections = [
-            new BtnComponent("Учить!", btn1Path, "btn-warning btn-lg"),
-            new BtnComponent("Тренировать", btn2Path, "btn-info btn-lg")
-    	];
+		$scope.sections = [
+			new BtnComponent("Учить!", btn1Path, "btn-warning btn-lg"),
+			new BtnComponent("Тренировать", btn2Path, "btn-info btn-lg"),
+		];
 
-    	IrregularVerbsData.query({}, function (data) {
-    		$scope.words = data.map(function (elem) {
-    			return {
-    				Ru: elem.Ru,
-    				Eng: elem["form 1"] + ", " + elem["form 2"] + ", " + elem["form 3"]
-    			}
-    		});
+		IrregularVerbsData.query({}, function(data) {
+			$scope.words = data.map(function(elem) {
+				return {
+					Ru: elem.Ru,
+					Eng: elem["form 1"] + ", " + elem["form 2"] + ", " + elem["form 3"],
+				};
+			});
 
-    		$scope.message = "Слов на сегодня " + $scope.words.length;
-    	});
-    }
+			$scope.message = "Слов на сегодня " + $scope.words.length;
+		});
+	},
 ]);
 
 memorizeApp.controller("listCtrl", [
-    "$scope", "$routeParams",
-    function ($scope, $routeParams) {
-    	$scope.main.isBrandNameShown = false;
-    	$scope.title = "Список уроков";
-    	$scope.message = "Выбирай, что сегодня выучим";
-    	var type = $routeParams.type;
+	"$scope",
+	"$routeParams",
+	function($scope, $routeParams) {
+		$scope.main.isBrandNameShown = false;
+		$scope.title = "Список уроков";
+		$scope.message = "Выбирай, что сегодня выучим";
+		var type = $routeParams.type;
 
-    	if (type === "lesson" || type === "training") {
-    		$scope.sections = [];
-    		_.map($scope.lessons, function (elem) {
-    			$scope.sections.push(new BtnComponent(
-				 "Урок " + elem,
-				 type === "lesson" ? "#/detail-view/lesson-" + elem : "#/learning/lesson-" + elem + "/training",
-				 type === "lesson" ? "btn-material-green btn-lg" : "btn-material-light-blue btn-lg"));
-    		});
-    	}
-    }
+		if (type === "lesson" || type === "training") {
+			$scope.sections = [];
+			_.map($scope.lessons, function(elem) {
+				$scope.sections.push(
+					new BtnComponent(
+						"Урок " + elem,
+						type === "lesson" ? "#/detail-view/lesson-" + elem : "#/learning/lesson-" + elem + "/training",
+						type === "lesson" ? "btn-material-green btn-lg" : "btn-material-light-blue btn-lg",
+					),
+				);
+			});
+		}
+	},
 ]);
 
 memorizeApp.controller("learningCtrl", [
-    "$scope", "$routeParams", "LessonsData", "IrregularVerbsData",
-    function ($scope, $routeParams, LessonsData, IrregularVerbsData) {
-    	var storeFileId = $routeParams.id,
+	"$scope",
+	"$routeParams",
+	"LessonsData",
+	"IrregularVerbsData",
+	function($scope, $routeParams, LessonsData, IrregularVerbsData) {
+		var storeFileId = $routeParams.id,
 			type = $routeParams.type;
 
-    	$scope.main.isBrandNameShown = false;
-    	$scope.title = setTitle(storeFileId);
-    	$scope.count = 0;
-    	$scope.currWord = $scope.count;
-    	reloadProgress($scope);
+		$scope.main.isBrandNameShown = false;
+		$scope.title = setTitle(storeFileId);
+		$scope.count = 0;
+		$scope.currWord = $scope.count;
+		reloadProgress($scope);
 
-    	if (storeFileId === "irregular-verbs") {
-    		IrregularVerbsData.query({}, function (data) {
-    			initWords($scope, data.map(function (elem) {
-    				return {
-    					Ru: elem.Ru,
-    					Eng: elem["form 1"] + ", " + elem["form 2"] + ", " + elem["form 3"]
-    				}
-    			}));
-    		});
-    	} else {
-    		LessonsData.query({ id: storeFileId }, function (data) {
-    			initWords($scope, data);
-    		});
-    	}
+		if (storeFileId === "irregular-verbs") {
+			IrregularVerbsData.query({}, function(data) {
+				initWords(
+					$scope,
+					data.map(function(elem) {
+						return {
+							Ru: elem.Ru,
+							Eng: elem["form 1"] + ", " + elem["form 2"] + ", " + elem["form 3"],
+						};
+					}),
+				);
+			});
+		} else {
+			LessonsData.query({ id: storeFileId }, function(data) {
+				initWords($scope, data);
+			});
+		}
 
-    	switch (type) {
-    		case "learn":
-    			$scope.isShown = true;
-    			navigateButtonHandler($scope);
-    			break;
-    		case "training":
-    			$scope.isShown = false;
-    			navigateButtonHandler($scope, function () {
-    				$scope.isShown = false;
-    			});
-    			$scope.show = function () {
-    				$scope.isShown = !$scope.isShown;
-    			};
-    			break;
-    	}
-    }
+		switch (type) {
+			case "learn":
+				$scope.isShown = true;
+				navigateButtonHandler($scope);
+				break;
+			case "training":
+				$scope.isShown = false;
+				navigateButtonHandler($scope, function() {
+					$scope.isShown = false;
+				});
+				$scope.show = function() {
+					$scope.isShown = !$scope.isShown;
+				};
+				break;
+		}
+	},
 ]);
--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,14 @@
 "use strict";
 
-var gulp = require("gulp"),
-    wiredep = require("wiredep").stream,
-    useref = require("gulp-useref"),
-    gulpif = require("gulp-if"),
-    uglify = require("gulp-uglify"),
-    clean = require("gulp-clean"),
-    minifyCss = require("gulp-minify-css");
+const gulp = require("gulp");
+const wiredep = require("wiredep").stream;
+const useref = require("gulp-useref");
+const gulpif = require("gulp-if");
+const uglify = require("gulp-uglify");
+const clean = require("gulp-clean");
+const minifyCss = require("gulp-minify-css");
 
-var path = {
+const path = {
 	indexFile: "./app/index.html",
 	scripts: "./app/**/*.js",
 	styles: "./app/**/*.css",
@@ -25,55 +25,55 @@ var path = {
 		images: "../www/images",
 		views: "../www/views",
 		store: "../www/store/",
-	}
+	},
 };
 
-gulp.task("copy-fonts", function () {
-	gulp.src(path.fonts)
-	.pipe(gulp.dest(path.dest.fonts	));
+gulp.task("copy-fonts", function() {
+	gulp.src(path.fonts).pipe(gulp.dest(path.dest.fonts));
 });
 
-gulp.task("copy-images", function () {
-	gulp.src(path.images)
-	.pipe(gulp.dest(path.dest.images));
+gulp.task("copy-images", function() {
+	gulp.src(path.images).pipe(gulp.dest(path.dest.images));
 });
 
-gulp.task("copy-views", function () {
-	gulp.src(path.views)
-	.pipe(gulp.dest(path.dest.views));
+gulp.task("copy-views", function() {
+	gulp.src(path.views).pipe(gulp.dest(path.dest.views));
 });
 
-gulp.task("copy-store", function () {
-	gulp.src(path.store)
-	.pipe(gulp.dest(path.dest.store));
+gulp.task("copy-store", function() {
+	gulp.src(path.store).pipe(gulp.dest(path.dest.store));
 });
 
-gulp.task("build", function () {
-    var assets = useref.assets();
+gulp.task("build", function() {
+	const assets = useref.assets();
 
-    return gulp.src("./app/*.html")
-        .pipe(assets)
-        .pipe(gulpif("*.js", uglify()))
-        .pipe(gulpif("*.css", minifyCss()))
-        .pipe(assets.restore())
-        .pipe(useref())
-        .pipe(gulp.dest(path.dest.root));
+	return gulp
+		.src("./app/*.html")
+		.pipe(assets)
+		.pipe(gulpif("*.js", uglify()))
+		.pipe(gulpif("*.css", minifyCss()))
+		.pipe(assets.restore())
+		.pipe(useref())
+		.pipe(gulp.dest(path.dest.root));
 });
 
-gulp.task("inline-bower", function () {
-    gulp.src(path.indexFile)
-        .pipe(wiredep({
-            directory : "app/bower_components"
-        }))
-        .pipe(gulp.dest("./app"));
+gulp.task("inline-bower", function() {
+	gulp
+		.src(path.indexFile)
+		.pipe(
+			wiredep({
+				directory: "app/bower_components",
+			}),
+		)
+		.pipe(gulp.dest("./app"));
 });
 
-gulp.task("watch", function () {
+gulp.task("watch", function() {
 	gulp.watch("bower.json", ["inline-bower"]);
-    gulp.watch(path.indexFile, ["build"]);
-    gulp.watch(path.views, ["build"]);
-    gulp.watch(path.scripts, ["build"]);
-    gulp.watch(path.styles, ["build"]);
+	gulp.watch(path.indexFile, ["build"]);
+	gulp.watch(path.views, ["build"]);
+	gulp.watch(path.scripts, ["build"]);
+	gulp.watch(path.styles, ["build"]);
 });
 
 gulp.task("copy-content", ["copy-images", "copy-fonts", "copy-views", "copy-store"]);
